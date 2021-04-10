9c84261a927632aff5c2399914a38362f38c2d77
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-04-10 17:19:58 -0400

    added dynamic html generation based on object parameters

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
-// const fs = require('fs');
-// const generatePage = require("./src/page-template.js");
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -111,12 +111,13 @@ const promptProject = (portfolioData) => {
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile("./index.html", pageHTML, (err) => {
+      if (err) throw err;
+
+      console.log(
+        "Page created! Check out index.html in thsi directory to see it!"
+      );
+    });
   });
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log("Portfolio complete! Check out index.html to see the output!");
-// });
